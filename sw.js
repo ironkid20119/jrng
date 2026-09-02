99287cbcf07bb405b7f4d656379d943a71074f8f
ironkid20119 <bruhfihs69@gmail.com>
2026-09-01 21:24:11 -0300

    Update CACHE_VERSION to junis-rng-shell-boi

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
    whether the page's own HTML/CSS/JS can load without a network connection.
    ============================================================ */
 
-const CACHE_VERSION = 'junis-rng-shell-v25';
+const CACHE_VERSION = 'junis-rng-shell-boi';
 const APP_SHELL = [
   './',
   './index.html',
